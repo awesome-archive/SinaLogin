44ad716fe0544654f96c11d83e9f3ad3cd9e7a7a
huiyadanli <huiyadanli@gmail.com>
2016-03-13 23:13:36 +0800

    初始化重构项目，原来的代码太丑了

---
+++ SinaLogin/Weibo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinaLogin
+{
+    public class Weibo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Weibo(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+    }
+}
