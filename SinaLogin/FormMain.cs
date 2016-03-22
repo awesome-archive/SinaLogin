8e2f7397cff67b22545e613a233cecca0882f900
huiyadanli <huiyadanli@gmail.com>
2016-03-22 23:08:39 +0800

    准备加入后台线程

--- SinaLogin/FormMain.cs
+++ SinaLogin/FormMain.cs
@@ -11,6 +11,11 @@ namespace SinaLogin
 {
     public partial class FormMain : Form
     {
+        //下一步
+        //把登录过程放在后台线程中
+        bool needPIN = false;
+        Weibo wb;
+
         public FormMain()
         {
             InitializeComponent();
@@ -18,15 +23,24 @@ namespace SinaLogin
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            Weibo wb = new Weibo(txtUsername.Text, txtPassword.Text);
-            Image pinImage = wb.StartLogin();
-            if (pinImage != null)
+            if (!needPIN)
             {
-                picPIN.Image = pinImage;
+                wb = new Weibo(txtUsername.Text, txtPassword.Text);
+                Image pinImage = wb.StartLogin();
+                if (pinImage != null)
+                {
+                    picPIN.Image = pinImage;
+                    needPIN = true;
+                }
+                else
+                {
+                    wb.EndLogin(null);
+                    txtRet.Text = wb.Get("http://weibo.com/guide/welcome");
+                }
             }
             else
             {
-                wb.EndLogin(null);
+                wb.EndLogin(txtPIN.Text);
                 txtRet.Text = wb.Get("http://weibo.com/guide/welcome");
             }
         }
